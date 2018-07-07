11ebfd9bc74e7c8093a1353b6a26638a34c554f5
Daniel <alabujadaniel@yahoo.com>
2018-07-07 19:33:57 +0100

    Link to medium post updated

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -40,7 +40,7 @@
                     <!-- Right Side Of Navbar -->
                     <ul class="navbar-nav ml-auto">
                         <!-- Authentication Links -->
-                        @guest
+                        @guest 
                             <li class="nav-item">
                                 <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
                             </li>
